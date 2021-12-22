923292ce20458100ef2204450956a6dbc27fce5a
gringbeeri <vladgashko@gmail.com>
2021-12-22 16:03:03 +0300

    fixed files in branch validation_models

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,12 +1,14 @@
 class Answer < ApplicationRecord
   belongs_to :question
 
-  scope :right, -> { where(correct: 't') }
+  scope :right, -> { where(correct: true) }
 
   validates :body, presence: true
-  validates :question_id, uniqueness: true, if: :validate_max_answer?
+  validate :number_answers
 
-  def validate_max_answer?
-    question_id > 1 && question_id < 4
+  def number_answers
+    Answer.where(question_id: Question.ids).each do |questions|
+      errors.add(:question_id, 'Maximum 3 values') if Answer.where(:question_id => questions.question_id).count > 4
+    end
   end
 end
--- app/models/test.rb
+++ app/models/test.rb
@@ -19,7 +19,6 @@ class Test < ApplicationRecord
       .order(id: :desc)
   }
 
-  validates :title, presence: true
-                    uniqueness: true
+  validates :title, presence: true, uniqueness: true
   validates :level, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 end
