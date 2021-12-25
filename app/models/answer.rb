5c04aec9ad07ace4d0d5d39d0e7a2ad92d13e711
gringbeeri <vladgashko@gmail.com>
2021-12-25 22:43:19 +0300

    fix file answer.rb

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -4,11 +4,14 @@ class Answer < ApplicationRecord
   scope :correct, -> { where(correct: true) }
 
   validates :body, presence: true
+  validates :question_id, presence: true
   validate :question_has_no_more_than_four_answers
 
+  private
+
   def question_has_no_more_than_four_answers
-    Question.where(id: Question.ids).each do |questions|
-      errors.add(:id, 'There are no more than 3 answers available') if Answer.where(:question_id => questions.id).count > 4
+    Question.ids.each do |questions|
+      errors.add(:question_id, 'There are no more than 3 answers available') if Answer.where(:question_id => questions).count > 4
     end
   end
 end
